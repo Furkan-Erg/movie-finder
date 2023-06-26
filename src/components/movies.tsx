ed750cf468e907b36b61d76a98f76a3c589e9727
Furkan Erguldurenler <furkanerguldurenler@hotmail.com>
2023-06-26 14:40:17 +0300

    Preparing the api calls

--- src/components/movies.tsx
+++ src/components/movies.tsx
@@ -1,10 +1,26 @@
-import React, { Component } from "react";
-
+import React, { Component, useState } from "react";
+import MovieService from "../services/movie.service";
+import { MovieModel, MovieResponse } from "../models/movieResponse";
 export default class movies extends Component {
+  movieService = new MovieService();
+  movies: MovieModel[] = [];
+  movieResponse: MovieResponse;
+  componentDidMount() {
+    this.getPopularMovies();
+  }
+
+  getPopularMovies() {
+    this.movieService.getPopularMovies().then((response) => {
+      this.movieResponse = response.data;
+      this.movies = this.movieResponse.results;
+      console.log(this.movies);
+    });
+  }
   render() {
     return (
       <div className="d-flex justify-content-center align-items-center flex-column">
         <h1 className="text-center">Title will be statefull</h1>
+        <button className="btn btn-primary ">Click to see :D</button>
         <div className="container d-flex justify-content-center align-items-center flex-column">
           <div className="row">
             <div className="col">
@@ -19,6 +35,20 @@ export default class movies extends Component {
                 </div>
               </div>
             </div>
+            {this.movies.map((movie) => (
+              <div className="col">
+                <div
+                  className="card m-2 "
+                  style={{ cursor: "pointer", width: "18rem", height: "18rem" }}
+                >
+                  <img src="" className="card-img-top" alt="images of movies" />
+                  <div className="card-body">
+                    <h5 className="card-title">Title {movie.title}</h5>
+                    <p className="card-text line-clamp">Overwiev</p>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
           <div>pagination will be here</div>
         </div>
---
+++ src/services/movie.service.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosResponse } from "axios";
+import { MovieResponse } from "../models/movieResponse";
+
+export default class movieService {
+  apiKey: string =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZGI1NTFmZWI5NjY3MWY4YjU5NDI2YWFlNGE1YmE5NSIsInN1YiI6IjYzZDIzNTM0OWY1MWFmMDBkYzI1ZjNmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pN5FakQlhRsQqUilleUN1F2aneXkoiNH-YwZUn_N2kc";
+  baseUrl: string = "https://api.themoviedb.org/3/";
+
+  api = axios.create({
+    baseURL: this.baseUrl,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + this.apiKey,
+    },
+  });
+  async getPopularMovies(): Promise<AxiosResponse<MovieResponse>> {
+    let url: string = "movie/popular";
+    return await this.api.get<MovieResponse>(url);
+  }
+  getNowPlayingMovies(): Promise<AxiosResponse<MovieResponse>> {
+    let url: string = "movie/now_playing";
+    return this.api.get<MovieResponse>(url);
+  }
+  getTopRatedMovies(): Promise<AxiosResponse<MovieResponse>> {
+    let url: string = "movie/top_rated";
+    return this.api.get<MovieResponse>(url);
+  }
+  getUpComingMovies(): Promise<AxiosResponse<MovieResponse>> {
+    let url: string = "movie/upcoming";
+    return this.api.get<MovieResponse>(url);
+  }
+  getMovieDetail(movieId: string): Promise<AxiosResponse<MovieResponse>> {
+    let url: string = "movie/" + movieId;
+    return this.api.get<MovieResponse>(url);
+  }
+
+  getMovieCredits(movieId: string): Promise<AxiosResponse<MovieResponse>> {
+    let url: string = "movie/" + movieId + "/credits";
+    return this.api.get<MovieResponse>(url);
+  }
+}
